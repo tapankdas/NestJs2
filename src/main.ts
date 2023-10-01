346ef574277e397f5fe0fb0c64e0536a2bb2ee36
tapankdas <tapan.kdas@gmail.com>
2023-10-02 00:43:03 +0530

    first commit

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    credentials:true,
+    origin:["http://localhost:3000","https://main.d2ikncomdr6rnr.amplifyapp.com"]
+  });
+  
+  app.use(cookieParser())
+  await app.listen(4000);
+}
+bootstrap();
